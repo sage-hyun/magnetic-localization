648f7f99c0053b974eebb29cd43e8306a311a360
sage-hyun <dan06052@naver.com>
2024-11-27 19:24:36 +0900

    Feat: 배경에 평면도 추가 및 비율 조정

--- app/src/main/java/com/example/magneticlocalization/GraphView.kt
+++ app/src/main/java/com/example/magneticlocalization/GraphView.kt
@@ -9,6 +9,9 @@ import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.View
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
 
 class GraphView @JvmOverloads constructor(
     context: Context,
@@ -28,14 +31,14 @@ class GraphView @JvmOverloads constructor(
 
     private val textPaint = Paint().apply {
         color = Color.WHITE
-        textSize = 30f
+        textSize = 10f
         textAlign = Paint.Align.CENTER
     }
 
     private val edgePaint = Paint().apply {
         color = Color.BLACK
         style = Paint.Style.STROKE
-        strokeWidth = 5f
+        strokeWidth = 1f
     }
 
     private val cursorPaint = Paint().apply {
@@ -53,7 +56,7 @@ class GraphView @JvmOverloads constructor(
     private val scaleGestureDetector = ScaleGestureDetector(context, object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
         override fun onScale(detector: ScaleGestureDetector): Boolean {
             scaleFactor *= detector.scaleFactor
-            scaleFactor = scaleFactor.coerceIn(0.1f, 3f) // 최소 0.1배, 최대 3배
+            scaleFactor = scaleFactor.coerceIn(0.2f, 5f) // 최소 0.2배, 최대 5배
             invalidate()
             return true
         }
@@ -63,31 +66,36 @@ class GraphView @JvmOverloads constructor(
         override fun onScroll(
             e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float
         ): Boolean {
-            offsetX -= distanceX
-            offsetY -= distanceY
+            offsetX -= distanceX / scaleFactor
+            offsetY -= distanceY / scaleFactor
             invalidate()
             return true
         }
     })
 
+    private var backgroundBitmap: Bitmap? = null
+
+    init {
+        // 배경 이미지 로드
+        backgroundBitmap = BitmapFactory.decodeResource(resources, R.drawable.floor_plan_1540) // 이미지 리소스 이름에 맞게 수정
+    }
+
     fun addNode(position: Pair<Int, Int>, magnitude: Int) {
 //        if (!nodes.any { it.first == position }) {
-            if (nodes.isNotEmpty()) {
-                val lastNode = nodes.last().first
-                edges.add(Pair(lastNode, position)) // 간선 추가
-            }
-            nodes.add(Triple(position, magnitude, magnitude.toString()))
-            invalidate()
+        if (nodes.isNotEmpty()) {
+            val lastNode = nodes.last().first
+            edges.add(Pair(lastNode, position)) // 간선 추가
+        }
+        nodes.add(Triple(position, magnitude, magnitude.toString()))
+        invalidate()
 //        }
     }
 
     fun updateCursor(position: Pair<Int, Int>) {
         cursor = position
 
-//        val (x, y) = mapToCanvas(position)
-        offsetX = -position.first * 150f
-        offsetY = position.second * 150f
-
+        offsetX = -position.first * 15.02f
+        offsetY = position.second * 15.02f
         invalidate()
     }
 
@@ -97,7 +105,10 @@ class GraphView @JvmOverloads constructor(
         // 캔버스에 스케일 및 이동 적용
         canvas.save()
         canvas.scale(scaleFactor, scaleFactor, width / 2f, height / 2f)
-        canvas.translate(offsetX / scaleFactor, offsetY / scaleFactor)
+        canvas.translate(offsetX, offsetY)
+
+        // 배경 이미지 그리기
+        drawBackground(canvas)
 
         // 간선 그리기
         edges.forEach { edge ->
@@ -115,10 +126,19 @@ class GraphView @JvmOverloads constructor(
         canvas.restore()
     }
 
+
+    private fun drawBackground(canvas: Canvas) {
+        backgroundBitmap?.let { bitmap ->
+            val (x, y) = mapToCanvas(Pair(0,0))
+            canvas.drawBitmap(bitmap, x-15f, y-15f, null) // 노드가 모눈 안에 들어오도록 보정
+        }
+    }
+
+
     private fun drawNode(canvas: Canvas, position: Pair<Int, Int>, text: String) {
         val (x, y) = mapToCanvas(position)
-        canvas.drawCircle(x, y, 50f, nodePaint) // 노드 원
-        canvas.drawText(text, x, y + 10f, textPaint) // 노드 텍스트
+        canvas.drawCircle(x, y, 10f, nodePaint) // 노드 원
+        canvas.drawText(text, x, y + 3.3f, textPaint) // 노드 텍스트
     }
 
     private fun drawEdge(canvas: Canvas, from: Pair<Int, Int>, to: Pair<Int, Int>) {
@@ -130,12 +150,14 @@ class GraphView @JvmOverloads constructor(
 
     private fun drawCursor(canvas: Canvas, position: Pair<Int, Int>) {
         val (x, y) = mapToCanvas(position)
-        canvas.drawCircle(x, y, 60f, cursorPaint)
+        canvas.drawCircle(x, y, 12f, cursorPaint)
     }
 
     private fun mapToCanvas(position: Pair<Int, Int>): Pair<Float, Float> {
-        val canvasX = width / 2f + position.first * 150f
-        val canvasY = height / 2f - position.second * 150f
+//        val canvasX = width / 2f + position.first * 15.02f
+//        val canvasY = height / 2f - position.second * 15.02f
+        val canvasX = width / 2f + position.first * 15.02f * 2f + offsetX
+        val canvasY = height / 2f - position.second * 15.02f * 2f  + offsetY
         return Pair(canvasX, canvasY)
     }
 
